20ebd2dde183b9edb53c03654e91afa7be4efedd
sametkula <sametkula@proton.me>
2023-07-15 00:43:31 +0300

    tying someting(code):

---
+++ com/sametkula/src/minefield/Field.java
@@ -0,0 +1,11 @@
+package com.sametkula.src.minefield;
+
+public class Field {
+    int col, row;
+    boolean isMine, flag;
+
+    public Field(int col, int row){
+        this.col = col;
+        this.row = row;
+    }
+}
--- com/sametkula/src/minefield/Minefield.java
+++ com/sametkula/src/minefield/Minefield.java
@@ -1,7 +1,36 @@
 package com.sametkula.src.minefield;
 
 
+import java.util.*;
 
 public class Minefield {
+    ArrayList<ArrayList<Field>> map;
+    int boardSize;
+
+    public Minefield(int boardSize){
+        this.boardSize = boardSize * boardSize;
+    }
+
+    public void run(){
+        prepareBoard();
+
+    }
+
+    private void prepareBoard(){
+        Random rd = new Random();
+
+        int height = boardSize / boardSize;
+        map = new ArrayList<>();
+
+        for (int i = 0; i < height; i++){
+            List dummyList = new ArrayList();
+            for (int j = 0; j < height; j++){
+                Field field = new Field(j,i);
+                dummyList.add(j, field);
+            }
+            map.add(dummyList);
+        }
+
+    }
 
 }
